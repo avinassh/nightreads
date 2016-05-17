a6749e771cbd63f62b3c8e6b0b17d31910d273fc
avi <hi@avi.im>
2016-05-17 21:33:27 +0530

    Add `Post` and `Tag` models

--- nightreads/posts/models.py
+++ nightreads/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from nightreads.utils import TimeStampMixin
+
+
+class Tag(TimeStampMixin):
+    name = models.CharField(max_length=20, unique=True)
+
+
+class Post(TimeStampMixin):
+    title = models.CharField(max_length=300)
+    url = models.URLField()
+
+    added_by = models.ForeignKey(User)
+    tags = models.ManyToManyField(Tag)
--- nightreads/settings.py
+++ nightreads/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'nightreads',
+    'nightreads.posts'
 ]
 
 MIDDLEWARE_CLASSES = [
