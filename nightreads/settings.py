113de3e5d6cb140a6df8426288fde665451f3ceb
avi <hi@avi.im>
2016-05-17 23:28:32 +0530

    Add Django app `user_manager`

--- nightreads/settings.py
+++ nightreads/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'nightreads',
-    'nightreads.posts'
+    'nightreads.posts',
+    'nightreads.user_manager'
 ]
 
 MIDDLEWARE_CLASSES = [
