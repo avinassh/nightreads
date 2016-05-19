1a2e102be0a355e27329ad26b2bc6339249e4c9f
avi <hi@avi.im>
2016-05-19 22:09:03 +0530

    Update `ConfirmForm` to validate properly

--- nightreads/user_manager/forms.py
+++ nightreads/user_manager/forms.py
@@ -15,14 +15,28 @@ class UnsubscribeForm(forms.Form):
     email = forms.EmailField()
 
 
-class ConfirmForm(forms.Form):
+class ConfirmEmailForm(forms.Form):
     email = forms.EmailField()
+    subscribe = forms.IntegerField()
     code = forms.CharField(max_length=80)
 
-    def clean_email(self):
-        email = self.cleaned_data['email']
+    def clean_subscribe(self):
+        value = bool(self.cleaned_data['subscribe'])
+        self.cleaned_data['subscribe'] = value
+        return value
+
+    def clean(self):
+        cleaned_data = super(ConfirmEmailForm, self).clean()
+        if self.errors:
+            return cleaned_data
+        email = cleaned_data['email']
+        code = cleaned_data['code']
         user = User.objects.filter(username=email).first()
         if not user:
             raise forms.ValidationError('Email not found')
         self.cleaned_data['user'] = user
-        return email
+        if user.emailverification.is_key_expired():
+            raise forms.ValidationError('Link expired, please regenerate')
+        if not user.emailverification.key == code:
+            raise forms.ValidationError('Invalid Link')
+        return cleaned_data
