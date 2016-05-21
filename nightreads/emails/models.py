fefe9a31ae169a0abf8550221b4ded33402a2644
avi <hi@avi.im>
2016-05-21 15:11:03 +0530

    Add `Email` model

--- nightreads/emails/models.py
+++ nightreads/emails/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from nightreads.utils import TimeStampMixin
+from nightreads.posts.models import Post
+
+
+class Email(TimeStampMixin):
+    subject = models.CharField(max_length=300)
+    message = models.TextField()
+    targetted_users = models.PositiveIntegerField(null=True)
+    is_sent = models.BooleanField(default=False)
+
+    post = models.ForeignKey(Post, null=True)
