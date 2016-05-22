2a5d4cb0a13a992489b8acc7b9411fe6c6874605
avi <hi@avi.im>
2016-05-22 16:02:39 +0530

    Add `SendEmailAdminView` for sending emails from admin

--- nightreads/emails/views.py
+++ nightreads/emails/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.core.urlresolvers import reverse
+from django.contrib import messages
 
-# Create your views here.
+from .models import Email
+from .forms import EmailAdminForm
+
+
+class SendEmailAdminView(View):
+
+    template = 'admin/emails/email/send_email.html'
+    form_class = EmailAdminForm
+
+    def get(self, request, pk):
+        email_obj = Email.objects.get(pk=pk)
+        return render(request, self.template, {'email_obj': email_obj})
+
+    def post(self, request, pk):
+        email_obj = Email.objects.get(pk=pk)
+        email_obj.is_sent = True
+        # send email
+        messages.add_message(request, messages.INFO, 'Email has been sent!')
+        return redirect(reverse(
+            'admin:emails_email_change', args=(email_obj.id,)))
