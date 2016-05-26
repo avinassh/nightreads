99cfdcd2165e000c9ac49d8d85714c909b61f741
avi <hi@avi.im>
2016-05-26 09:00:05 +0530

    Add send `Preview` email button

--- nightreads/emails/views.py
+++ nightreads/emails/views.py
@@ -17,9 +17,15 @@ class SendEmailAdminView(View):
         return render(request, self.template, {'email_obj': email_obj})
 
     def post(self, request, pk):
+        email_type = request.POST.get('type', '').lower()
         email_obj = Email.objects.get(pk=pk)
-        email_obj.is_sent = True
-        # send email
-        messages.add_message(request, messages.INFO, 'Email has been sent!')
+        if email_type == 'preview':
+            # send preview email
+            m = 'Preview email has been sent!'
+        else:
+            # send email
+            m = 'Email has been sent!'
+            email_obj.is_sent = True
+        messages.add_message(request, messages.INFO, m)
         return redirect(reverse(
             'admin:emails_email_change', args=(email_obj.id,)))
