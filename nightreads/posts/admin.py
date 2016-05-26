60aa2073ba646e8d0131f33d8fbd95aaf64d7ea4
avi <hi@avi.im>
2016-05-26 23:55:45 +0530

    Display count of subscribers and posts

--- nightreads/posts/admin.py
+++ nightreads/posts/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from .models import Post, Tag
 
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_subscribers', 'get_posts')
+
+    def get_subscribers(self, obj):
+        return obj.subscription_set.count()
+    get_subscribers.short_description = 'Subscribers'
+
+    def get_posts(self, obj):
+        return obj.post_set.count()
+    get_posts.short_description = 'Posts'
+
 admin.site.register(Post)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
