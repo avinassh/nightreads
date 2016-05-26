b1bf663d4e752b1c137e846e9928fc55004f7951
avi <hi@avi.im>
2016-05-26 09:10:13 +0530

    Update settings

--- nightreads/settings/common.py
+++ nightreads/settings/common.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_ROOT)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -119,7 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
@@ -137,6 +136,9 @@ EMAIL_LINK_EXPIRY_DAYS = 2 * 86400
 
 # From Email to be used in sending mails
 SENDER_EMAIL = 'nightreads@devup.in'
+
+# List of email recipients who will receive preview email
+PREVIEW_RECEPIENTS = ('hi@avi.im', 'v@devup.in')
 
 # Summernote Settings
 SUMMERNOTE_CONFIG = {
