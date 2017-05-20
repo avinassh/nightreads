f77c28299bd1513aa939ee333b26f87db91e2aaf
avi <hi@avi.im>
2017-05-20 19:21:06 +0530

    Add `is_subscribed` list filter

--- nightreads/user_manager/admin.py
+++ nightreads/user_manager/admin.py
@@ -5,5 +5,6 @@ from .models import Subscription
 
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'is_subscribed']
+    list_filter = ['is_subscribed']
 
 admin.site.register(Subscription, SubscriptionAdmin)
