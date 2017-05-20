e06061ff6ebb93496d54142188ae80637639a487
avi <hi@avi.im>
2017-05-20 22:14:32 +0530

    Update configs

--- nightreads/settings/sample_secrets.py
+++ nightreads/settings/sample_secrets.py
@@ -21,6 +21,6 @@ DB_SETTINGS = {
     'NAME': 'DB_NAME',
     'USER': 'DB_USERNAME',
     'PASSWORD': 'DB_PASSWORD',
-    'HOST': 'DB_HOST',
-    'PORT': 'DB_PORT'
+    'HOST': 'localhost',
+    'PORT': ''
 }
